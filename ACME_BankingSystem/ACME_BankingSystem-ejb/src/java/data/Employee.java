62f28c067c762ad2aab5f0d799abbe390d746ccf
Peter <peterpavloman@gmail.com>
2013-08-20 15:12:58 +1000

    Work on Employee / Savings.
    Signed-off-by: Peter <s3286430@student.rmit.edu.au>

--- ACME_BankingSystem/ACME_BankingSystem-ejb/src/java/data/Employee.java
+++ ACME_BankingSystem/ACME_BankingSystem-ejb/src/java/data/Employee.java
@@ -1,14 +1,68 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
 /**
- *
- * @author narks
+ * Employee object used by application logic.
+ * 
+ * @author Peter (s3286430
  */
-public class Employee
+public class Employee 
 {
+    private int mIDEmployee;
+    private String mFirstName;
+    private String mLastName;
+    
+    /**
+     * Constructor for Employee class.
+	 * Initializes default required values.
+     * 
+     * @param aFirstName
+     * @param aLastName
+     */
+    public Employee(String aFirstName, String aLastName)
+    {
+        mFirstName = aFirstName;
+        mLastName = aLastName;
+    }
+    
+    /**
+     * 
+     * @return
+     */
+    public String getFirstName() { return mFirstName; }
+    /**
+     * Mutator for FirstName.
+     * Note that changes must be explicitly saved via a data access object.
+     * @param aFirstName
+     * @see getFirstName()
+     */
+    public void setFirstName(String aFirstName)
+    {
+        mFirstName = aFirstName;
+    }
+    /**
+     *
+     * @return
+     */
+    public String getLastName() { return mLastName; }
+    /**
+     * Mutator for LastName.
+     * Note that changes must be explicitly saved via a data access object.
+     * @param aLastName
+     * @see getLastName()
+     */
+    public void setLastName(String aLastName) 
+    {
+        mLastName = aLastName;
+    }
+	public int getIDEmployee() { return mIDEmployee; }
+    /**
+     *
+     * @param aIDEmployee
+     */
+    public void setIDEmployee(int aIDEmployee)
+    {
+        mIDEmployee = aIDEmployee;
+    }
     
 }
+
