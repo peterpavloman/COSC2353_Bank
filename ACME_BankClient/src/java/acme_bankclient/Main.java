7f5dffe41bd5c6d0e30cd6d05055ba0aecba9ab7
s3316048 <s3316048@student.rmit.edu.au>
2013-08-25 22:28:35 +1000

    Signed-off-by: s3316048 <s3316048@student.rmit.edu.au> add some codes in main class

--- ACME_BankClient/src/java/acme_bankclient/Main.java
+++ ACME_BankClient/src/java/acme_bankclient/Main.java
@@ -4,9 +4,9 @@
  */
 package acme_bankclient;
 
-import beans.CustomerBeanRemote;
-import beans.EmployeeBeanRemote;
-import beans.StatefulTestBeanRemote;
+//import beans.CustomerBeanRemote;
+//import beans.EmployeeBeanRemote;
+//import beans.StatefulTestBeanRemote;
 import exceptions.ApplicationLogicException;
 import exceptions.LoginFailureException;
 import java.io.BufferedReader;
@@ -15,19 +15,21 @@ import java.io.InputStreamReader;
 import java.util.List;
 import javax.ejb.EJB;
 import security.LoginSession;
+import beans.SavingsClientBeanRemote;
 
 /**
  *
  * @author Peter (s3286430)
  */
 public class Main {
-
-    @EJB
-    private static CustomerBeanRemote msCustomerBean;
-	@EJB
-	private static StatefulTestBeanRemote msStatefulTestBean;
-	@EJB
-	private static EmployeeBeanRemote msEmployeeBean;
+//    @EJB private static SavingsClientBeanRemote msSavingBean;
+//    @EJB
+//    private static SavingsClientBeanRemote msSavingBean;
+//    private static CustomerBeanRemote msCustomerBean;
+//	@EJB
+//	private static StatefulTestBeanRemote msStatefulTestBean;
+//	@EJB
+//	private static EmployeeBeanRemote msEmployeeBean;
     
     private static int testGetSelection()
     {
@@ -47,7 +49,7 @@ public class Main {
 					continue;
 				}
 				lOutput = Integer.parseInt(lInput);
-				if (lOutput <= 0 || lOutput > 7)
+				if (lOutput <= 0 || lOutput > 6)
 				{
 					continue;
 				}
@@ -60,8 +62,10 @@ public class Main {
             }
             catch (NumberFormatException lNFException)
             {
-				lNFException.printStackTrace();
+				//lNFException.printStackTrace();
                 lValidInput = false;
+                System.out.println("Please input a number for menu option.");
+                continue;
             }
         } while (lValidInput == false);
 		
@@ -70,46 +74,89 @@ public class Main {
     
     private static void testPrintMenu()
     {	
+        String loginId="";
+        String pwd="";
+        boolean flag=false;
+        BufferedReader lReadInput = new BufferedReader(new InputStreamReader(System.in));
+        do{
+            try {
+                System.out.println("Login Employee:");
+                System.out.println("Login ID:");
+                loginId=lReadInput.readLine();
+                System.out.println("Login Password:");
+                pwd=lReadInput.readLine();
+                //for test
+                if(loginId.equals(pwd)){
+                    flag=true;
+                    System.out.println("Login Successful!");
+                    System.out.println();
+                }
+                else
+                {
+                    System.out.println("Login fail!");
+                    System.out.println();
+                }
+//                if(msSavingBean.login(Integer.parseInt(loginId), pwd)){
+//                    flag=true;
+//                    System.out.println("Login Successful!");
+//                    System.out.println();
+//                }
+//                else
+//                {
+//                    System.out.println("Login fail!");
+//                    System.out.println();
+//                }
+            } catch (IOException ex) {
+                //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println(ex.getMessage());
+            }
+        }while(!flag);
 		while (true)
 		{
-			System.out.println("1: Add item 1");
-			System.out.println("2: Add item 2");
-			System.out.println("3: Add item 3");
-			System.out.println("4: Add item 4");
-			System.out.println("5: View cart items");
-			System.out.println("6: Empty cart items");
-			System.out.println("7: Log out");
+			System.out.println("1: Create a customer");
+			System.out.println("2: Open a Savings account");
+			System.out.println("3: Make deposit into Savings account");
+			System.out.println("4: Make withdrawal from Savings account");
+			System.out.println("5: View balance of Savings account");
+			System.out.println("6: Log out");
 			
 			int lSelection = testGetSelection();
 			
-			if (lSelection >= 1 && lSelection <= 4)
+			if (lSelection == 1 )
 			{
-				try
-				{
-					msStatefulTestBean.addItemToCart(lSelection);
-					System.out.println("Adding item " + lSelection);
-				}
-				catch (ApplicationLogicException lException)
-				{
-					System.out.println("An error has occurred: " +
-							lException.getUserMessage());
-				}
+//				try
+//				{
+//					msStatefulTestBean.addItemToCart(lSelection);
+//					System.out.println("Adding item " + lSelection);
+//				}
+//				catch (ApplicationLogicException lException)
+//				{
+//					System.out.println("An error has occurred: " +
+//							lException.getUserMessage());
+//				}
 			}
+                        else if (lSelection == 2)
+                        {
+                            
+                        }
+                        else if (lSelection == 3)
+                        {
+                            
+                        }
+                        else if (lSelection == 4)
+                        {
+                            
+                        }
 			else if (lSelection == 5)
 			{
-				List<String> lItems = msStatefulTestBean.getItemsInCart();
+//				List<String> lItems = msStatefulTestBean.getItemsInCart();
 				System.out.println("Items in cart: ");
-				for (String lItem : lItems)
-				{
-					System.out.println(lItem);
-				}
+//				for (String lItem : lItems)
+//				{
+//					System.out.println(lItem);
+//				}
 			}
 			else if (lSelection == 6)
-			{
-				msStatefulTestBean.emptyCart();
-				System.out.println("Cart emptied!");
-			}
-			else if (lSelection == 7)
 			{
 				System.out.println("Bye!");
 				break;
@@ -131,17 +178,13 @@ public class Main {
         
         System.out.println("Application client success!");
         */
-		try
-		{
-			System.out.println("Attempting login into Employee bean");
-			LoginSession lSession = msEmployeeBean.login(0, "legit");
-			System.out.println("Attempting login into Customer bean");
-			msCustomerBean.login(lSession);
-		}
-		catch (LoginFailureException aException)
-		{
-			System.out.println("Login failed: " + aException.getUserMessage());
-		}
+		
+//			System.out.println("Attempting login into Employee bean");
+//			LoginSession lSession = msEmployeeBean.login(0, "legit");
+//			System.out.println("Attempting login into Customer bean");
+//			msCustomerBean.login(lSession);
+		
+		
 		
 		testPrintMenu();
     }
